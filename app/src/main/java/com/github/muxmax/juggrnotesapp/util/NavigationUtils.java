411a78b7ff7c05b7291bd4343110348d142b8ee8
Max Wielsch <max.wielsch@googlemail.com>
2015-03-22 15:22:50 +0100

    Photos can be loaded, displayed and removed.

--- app/src/main/java/com/github/muxmax/juggrnotesapp/util/NavigationUtils.java
+++ app/src/main/java/com/github/muxmax/juggrnotesapp/util/NavigationUtils.java
@@ -1,15 +1,123 @@
 package com.github.muxmax.juggrnotesapp.util;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import com.github.muxmax.juggrnotesapp.view.NoteDetailActivity;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A utility class that provides navigation between different app parts. This decouples all views
  * (activities and fragments) from each other.
  */
 public class NavigationUtils {
+
+    private static final String APP_DIR = Environment
+            .getExternalStorageDirectory().getAbsolutePath()
+            .concat("/Android/data/com.github.muxmax.juggrnotesapp/images/");
+    public static final int RESULT_LOAD_IMAGE = 1;
+
+    /**
+     * Navigates to an appropriate activity that provides image resources. So a chooser intent will
+     * be started that shows gallery and photo apps.
+     *
+     * @param activity The current activity. Needed for starting further intents.
+     */
+    public static Uri goToGalleryOrPhotoApp(Activity activity) {
+
+        final File root = new File(
+                APP_DIR);
+        Uri capturedImageUri = null;
+
+        if (!root.mkdirs() && !root.isDirectory()) {
+            Log.e(NavigationUtils.class.getName(),
+                    "Could not create image file path for storing image from photo intent.");
+        } else {
+            final File newImageFilePath =
+                    new File(root, "juggr_note_app_img_" + System.currentTimeMillis() + ".jpg");
+            capturedImageUri = Uri.fromFile(newImageFilePath);
+
+            final List<Intent> cameraIntents = new ArrayList<>();
+            final Intent captureIntent =
+                    new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            final PackageManager packageManager = activity.getPackageManager();
+            final List<ResolveInfo> listCam =
+                    packageManager.queryIntentActivities(captureIntent, 0);
+            for (ResolveInfo res : listCam) {
+                final String packageName = res.activityInfo.packageName;
+                final Intent intent = new Intent(captureIntent);
+                intent.setComponent(
+                        new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+                intent.setPackage(packageName);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, capturedImageUri);
+                cameraIntents.add(intent);
+            }
+
+            // Filesystem.
+            final Intent galleryIntent = new Intent();
+            galleryIntent.setType("image/*");
+            galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+            // Chooser of filesystem options.
+            final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
+
+            // Add the camera options.
+            chooserIntent
+                    .putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                            cameraIntents.toArray(new Parcelable[0]));
+            activity.startActivityForResult(chooserIntent, RESULT_LOAD_IMAGE);
+        }
+        return capturedImageUri;
+    }
+
+    /**
+     * Tells whether the given intent data comes from a camera activity or not.
+     *
+     * @param data An intent possibly containing result data.
+     * @return {@code true} when the intent comes from a camera activity, {@code false} otherwise.
+     */
+    public static boolean isFromCamera(Intent data) {
+        if (data == null) {
+            return true;
+        } else {
+            final String action = data.getAction();
+            return action != null &&
+                    action.equals(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        }
+    }
+
+    /**
+     * Get the file path for the given uri.
+     *
+     * @param uri      An uri containing a file path to a media resource.
+     * @param activity The current activity.
+     * @return A string containing the path.
+     */
+    public static String getPath(Uri uri, Activity activity) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = activity.getContentResolver().query(uri, projection, null, null, null);
+
+        if (cursor == null) { // local file paths like Dropbox etc.
+            return uri.getPath();
+        }
+
+        cursor.moveToFirst();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        return cursor.getString(column_index);
+    }
 
     /**
      * Navigate to the {@link com.github.muxmax.juggrnotesapp.view.NoteDetailActivity}.
@@ -21,4 +129,5 @@ public class NavigationUtils {
         intent.putExtra(BundleArguments.NOTE_ID, noteId);
         context.startActivity(intent);
     }
+
 }
--- app/src/main/java/com/github/muxmax/juggrnotesapp/view/NoteDetailActivity.java
+++ app/src/main/java/com/github/muxmax/juggrnotesapp/view/NoteDetailActivity.java
@@ -1,14 +1,30 @@
 package com.github.muxmax.juggrnotesapp.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.muxmax.juggrnotesapp.R;
 import com.github.muxmax.juggrnotesapp.util.BundleArguments;
+import com.github.muxmax.juggrnotesapp.util.NavigationUtils;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 /**
  * An activity that shows detail information on a given note.
@@ -18,18 +34,63 @@ public class NoteDetailActivity extends ActionBarActivity {
     // model state
     private Long noteId;
 
+    // view
+    private ImageView imageView;
+    private ImageButton buttonDeletePhoto;
+    private EditText editTextTitle;
+    private EditText editTextContent;
+
+    // view state
+    private Uri capturedImageUri;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (stateCouldBeLoaded(savedInstanceState)) {
-
             initializeView();
-
         } else {
             handleLoadingError();
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NavigationUtils.RESULT_LOAD_IMAGE && resultCode == RESULT_OK)  {
+            if (!NavigationUtils.isFromCamera(data) && data == null) {
+                onErrorTakingAPhoto();
+            } else {
+                onLoadTakenPhoto(data);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.note_detail_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_take_photo) {
+            onActionTakeAPhoto();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     /**
      * Initialize all views of the activity.
@@ -38,10 +99,23 @@ public class NoteDetailActivity extends ActionBarActivity {
         setContentView(R.layout.note_detail_activity);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        buttonDeletePhoto = (ImageButton) findViewById(R.id.buttonDeletePhoto);
+        buttonDeletePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageBitmap(null);
+                editTextContent.setMaxLines(Integer.MAX_VALUE);
+                buttonDeletePhoto.setVisibility(View.GONE);
+            }
+        });
+
+        editTextContent = (EditText) findViewById(R.id.editTextContent);
+        editTextTitle = (EditText) findViewById(R.id.editTextTitle);
     }
 
     /**
@@ -71,26 +145,50 @@ public class NoteDetailActivity extends ActionBarActivity {
         finish();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.note_detail_menu, menu);
-        return true;
+    /**
+     * Should be called to signal that a photo activity should be notified to start.
+     */
+    private void onActionTakeAPhoto() {
+        capturedImageUri = NavigationUtils.goToGalleryOrPhotoApp(this);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * Load the photo from the returned intent into the proper image view.
+     * @param data An {@link android.content.Intent} containing data of of the taken photo.
+     */
+    private void onLoadTakenPhoto(Intent data) {
+        String imagePath = NavigationUtils.isFromCamera(data) ?
+                capturedImageUri.getPath() :
+                NavigationUtils.getPath(data.getData(), this);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point p = new Point(0, 0);
+        display.getSize(p);
 
-        return super.onOptionsItemSelected(item);
+        Picasso.with(this).load(new File(imagePath))
+                // actually we needed here to put in a proportional y value calculated from the image ratio.
+                .resize(p.x, p.y)
+                .into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        editTextContent.setMaxLines(11);
+                        buttonDeletePhoto.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+    }
+
+    /**
+     * Handle the error that occurred while taking a photo.
+     */
+    private void onErrorTakingAPhoto() {
+        Log.e(getClass().getName(),
+                "Error: The photo activity could not return an image.");
+        Toast.makeText(this, R.string.the_photo_could_not_be_taken, Toast.LENGTH_LONG);
     }
 }
